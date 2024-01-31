0afad50b8e850a26c3d1152e28e2ccd725c2bea7
yuta.fujisawa <mt.fuji1009@gmail.com>
2024-01-31 20:01:34 +0900

    add paramsにonetime_passwordが存在する場合に成功処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,20 @@ class PostsController < ApplicationController
   def create
     @user = User.find_by(uuid: params[:id])
     @post = Post.new(content: params[:content])
-    @post.onetime_password = params[:onetime_password]
-    flash[:alert] = t('ワンタイムパスワードが必要です。ワンタイムパスワードはcookieに保存しておきました。nameはpost[onetime_password]としてこのフォームに埋め込んで送信してください。あえて入力フォームは用意していませんので開発者ツールで無理やり作ってください。')
-    render :new
+    @post.onetime_password = params[:post][:onetime_password]
+    if @post.onetime_password
+      @post.save!
+      redirect_to posts_path
+    else
+      @post.onetime_password.nil?
+      flash[:alert] = t('ワンタイムパスワードが必要です。ワンタイムパスワードはcookieに保存しておきました。nameはpost[onetime_password]としてこのフォームに埋め込んで送信してください。あえて入力フォームは用意していませんので開発者ツールで無理やり作ってください。')
+      redirect_to new_post_path(user_uuid: @user.uuid)
+    end
   end
+end
 
-  private
+private
 
-  def post_params
-    params.require(:post).permit(:content, :onetime_password)
-  end
+def post_params
+  params.require(:post).permit(:content, :onetime_password)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   get 'start', to: 'welcome#start'
   resources :users, only: [:index, :create, :show]
   get 'users/activation_help', to: 'users#activation_help'
-  # /users/:uuid/authenticate に対するルーティング
   post '/users/:id/authenticate', to: 'users#authenticate', as: 'user_authenticate'
-  resources :posts, only: [:new]
-  post '/post/:id/new', to: 'posts#create', as: 'create_post'
+  resources :posts, only: [:index, :new]
+  post '/posts/:id/new', to: 'posts#create', as: 'create_post'
 end
 
