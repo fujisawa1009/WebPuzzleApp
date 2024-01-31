8439097b719ddb9dd4e07f620dacd72244f5446a
yuta.fujisawa <mt.fuji1009@gmail.com>
2024-01-31 20:18:18 +0900

    add cookieにonetime_passwordを埋め込んで判定する処理を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,23 @@
 class PostsController < ApplicationController
+
   def new
+    @post = Post.new
+    onetime_password = generate_onetime_password # このメソッドは適切に実装する必要があります
+    cookies[:onetime_password] = {
+      value: onetime_password,
+      expires: 1.hour.from_now, # 適切な有効期限を設定
+    }
   end
 
   def create
     @user = User.find_by(uuid: params[:id])
     @post = Post.new(content: params[:content])
     @post.onetime_password = params[:post][:onetime_password]
-    if @post.onetime_password
+    if @post.onetime_password == cookies[:onetime_password]
       @post.save!
       redirect_to posts_path
     else
-      @post.onetime_password.nil?
+      @post.onetime_password.nil?　|| @post.onetime_password != cookies[:onetime_password]
       flash[:alert] = t('ワンタイムパスワードが必要です。ワンタイムパスワードはcookieに保存しておきました。nameはpost[onetime_password]としてこのフォームに埋め込んで送信してください。あえて入力フォームは用意していませんので開発者ツールで無理やり作ってください。')
       redirect_to new_post_path(user_uuid: @user.uuid)
     end
@@ -21,4 +28,8 @@ private
 
 def post_params
   params.require(:post).permit(:content, :onetime_password)
+end
+
+def generate_onetime_password
+  SecureRandom.hex(16) # 16バイトのランダムな16進数文字列を生成する例
 end
