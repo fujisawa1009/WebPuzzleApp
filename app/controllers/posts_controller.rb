99e48e60f6c54091df4eef82748d8e71686dadf1
yuta.fujisawa <mt.fuji1009@gmail.com>
2024-02-13 18:03:50 +0900

    fix  @postの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,14 +14,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(uuid: params[:id])
-    @post = Post.new(content: params[:content])
+    @user = User.find_or_create_by(uuid: params[:id])
+    @post = @user.posts.build(content: params[:content])
     @post.onetime_password = params[:post][:onetime_password]
     if @post.onetime_password == cookies[:onetime_password]
       @post.save!
       redirect_to posts_path
     else
-      @post.onetime_password.nil? 　 || @post.onetime_password != cookies[:onetime_password]
+      @post.onetime_password.nil? || @post.onetime_password != cookies[:onetime_password]
       flash[:alert] = t('ワンタイムパスワードが必要です。ワンタイムパスワードはcookieに保存しておきました。nameはpost[onetime_password]としてこのフォームに埋め込んで送信してください。あえて入力フォームは用意していませんので開発者ツールで無理やり作ってください。')
       redirect_to new_post_path(user_uuid: @user.uuid)
     end
